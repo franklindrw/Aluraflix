fedf224b5f5877a1c1189ed88264af9f61d0e665
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-09-19 22:48:21 -0300

    finalizando o projeto

--- app.js
+++ app.js
@@ -3,41 +3,26 @@ function adicionarManga(){
     var nomeDoManga = document.getElementById("nomeManga").value;
     var urlDaImagem = document.getElementById("urlManga").value;
     var categoriaManga = document.getElementById("categoria").value;
+    var categoria = "listaDe" + categoriaManga;;
 
-    console.log(categoriaManga);
 
 // verificacoes dos campos
     if(nomeDoManga === "" || urlDaImagem === "" || categoriaManga === "Categoria"){
-        console.error("Favor preencha todos os campos");
-    }
-    else if(urlDaImagem.endsWith(".jpg") || urlDaImagem.endsWith(".png")){
-        listarMangas();
-    }
-    else{
-        console.error("Link de imagem inválido");
-    }
+         console.error("Favor preencha todos os campos");
+     }
+     else if(urlDaImagem.endsWith(".jpg") || urlDaImagem.endsWith(".png")){
 
+     //após a verificação, recebe as variaveis e monta a imagem com o título e adiciona conforme a categoria selecionada
+    var elementoManga = "<figure><img src=" + urlDaImagem + "><figcaption>" + nomeDoManga + "</figcaption></figure>";
+    var listarManga = document.getElementById(categoria);
+    listarManga.innerHTML = listarManga.innerHTML + elementoManga;
+
+     }
+     else{
+         console.error("Link de imagem inválido");
+     }
+
+    //por fim, limpa os campos para adicionar mais quadrinhos
     document.getElementById("nomeManga").value = "";
     document.getElementById("urlManga").value = "";
-}
-
-// monta o campo HTML para reber a imagem e o nome
-function listarMangas(nomeDoManga, urlDaImagem, categoriaManga){
-    var elementoManga = "<figure><img src=" + urlDaImagem + "><figcaption>" + nomeDoManga + "</figcaption></figure>";
-
-    console.log(categoriaManga);
-    if(categoriaManga === "Acao"){
-        var listaManga = document.getElementById("listaDeAcao");
-    }
-    else if(categoriaManga === "Suspense"){
-        var listaManga = document.getElementById("listaDeSuspense");
-    }
-    else if(categoriaManga === "Aventura"){
-        var listaManga = document.getElementById("listaDeAventura");
-    }
-    else{
-        var listaManga = document.getElementById("listaDeNovel");
-    }
-    
-    listaManga.innerHTML = listaManga.innerHTML + elementoManga;
 }
